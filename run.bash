52188441a4c3f3872a957fa478440e752197c521
enpolat <enpolat@microsoft.com>
2021-03-23 11:47:08 +0300

    push the tagged image to github container registry

--- run.bash
+++ run.bash
@@ -3,3 +3,5 @@ docker build -t copenhagen .
 docker tag copenhagen ghcr.io/polatengin/copenhagen:latest
 
 echo $COPENHAGEN_PAT | docker login ghcr.io -u polatengin --password-stdin
+
+docker push ghcr.io/polatengin/copenhagen:latest
